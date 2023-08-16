a3ddd0adcc0cc3cf9bdca3bce883fd0ada86b7bf
svolsch <adityadhiman912@gmail.com>
2023-08-16 12:57:28 +0100

    Initial commit Basic Chatbot

---
+++ gpt_based_tutor.py
@@ -0,0 +1,17 @@
+import openai
+
+
+messages = [ {"role": "system", "content":
+			"You are a intelligent assistant."} ]
+while True:
+	message = input("User : ")
+	if message:
+		messages.append(
+			{"role": "user", "content": message},
+		)
+		chat = openai.ChatCompletion.create(
+			model="gpt-3.5-turbo", messages=messages
+		)
+	reply = chat.choices[0].message.content
+	print(f"ChatGPT: {reply}")
+	messages.append({"role": "assistant", "content": reply})
